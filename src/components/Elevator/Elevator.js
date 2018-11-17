837fe325f61e6cc17b4d28ebad982a78172408e4
Jennifer Hurd <jenhurd8@gmail.com>
2018-11-16 21:23:57 -0600

    starting to work on adding gradient to borders

--- src/components/Elevator/Elevator.js
+++ src/components/Elevator/Elevator.js
@@ -67,92 +67,94 @@ class Elevator extends Component {
     const doorStatus = this.state.currentFloor === this.state.destinationFloor;
 
     return (
-      <div className="elevator__parentContainer">
+      <div>
         {/* <div className="elevator__parentContainerTop">Top</div>
         <div className="elevator__parentContainerBottom">Bottom</div>
         <div className="elevator__parentContainerRight">Right</div>
         <div className="elevator__parentContainerLeft">Left</div> */}
-        <div className="elevator__centeredElevatorAndControlsBox">
-          <div className="elevator__spaceOverElevator">
-            <div id="elevator__currentFloor">
-              <h2>{this.state.emergencyMessage}</h2>
-              <h3>{this.state.message}</h3>
+        <div className="elevator__parentContainer">
+          <div className="elevator__centeredElevatorAndControlsBox">
+            <div className="elevator__spaceOverElevator">
+              <div id="elevator__currentFloor">
+                <h2>{this.state.emergencyMessage}</h2>
+                <h3>{this.state.message}</h3>
+              </div>
             </div>
-          </div>
 
-          <div className="elevator__elevator">
-            <ElevatorDoor
-              doorStatus={doorStatus}
-              currentFloor={this.state.currentFloor}
-            />
-            <div className="elevator__controls">
-              <div id="elevator__currentFloorNumber">
-                <div id="elevator__floorIndicator">
-                  <h3>Current Floor:</h3>
-                  <p>{this.state.currentFloor}</p>
-                </div>
-              </div>
-              <div id="elevator__numbers">
-                <div className="elevator__halfNumbers2">
-                  <BaseButton
-                    clickAction={() => {
-                      this.setDestinationFloor(2);
-                      this.floorHandler();
-                    }}
-                    value={2}
-                  />
-                  <BaseButton
-                    clickAction={() => {
-                      this.setDestinationFloor(1);
-                      this.floorHandler();
-                    }}
-                    value={1}
-                  />
-                  <BaseButton
-                    clickAction={() => {
-                      this.setDestinationFloor(0);
-                      this.floorHandler();
-                    }}
-                    value={"G0"}
-                  />
-                </div>
-                <div className="elevator__halfNumbers1">
-                  <BaseButton
-                    clickAction={() => {
-                      this.setDestinationFloor(5);
-                      this.floorHandler();
-                    }}
-                    value={5}
-                  />
-                  <BaseButton
-                    clickAction={() => {
-                      this.setDestinationFloor(4);
-                      this.floorHandler();
-                    }}
-                    value={4}
-                  />
-                  <BaseButton
-                    clickAction={() => {
-                      this.setDestinationFloor(3);
-                      this.floorHandler();
-                    }}
-                    value={3}
-                  />
-                </div>
-              </div>
-              <br />
-              <BaseButton
-                color={"red"}
-                clickAction={() => this.emergencyMessage()}
-                value={"EMERGENCY"}
-                baseButtonBorderRadius={"5%"}
+            <div className="elevator__elevator">
+              <ElevatorDoor
+                doorStatus={doorStatus}
+                currentFloor={this.state.currentFloor}
               />
-              <BaseButton
-                color={"green"}
-                clickAction={() => this.resetMessage()}
-                value={"RESET MESSAGES"}
-                baseButtonBorderRadius={"5%"}
-              />
+              <div className="elevator__controls">
+                <div id="elevator__currentFloorNumber">
+                  <div id="elevator__floorIndicator">
+                    <h3>Current Floor:</h3>
+                    <p>{this.state.currentFloor}</p>
+                  </div>
+                </div>
+                <div id="elevator__numbers">
+                  <div className="elevator__halfNumbers2">
+                    <BaseButton
+                      clickAction={() => {
+                        this.setDestinationFloor(2);
+                        this.floorHandler();
+                      }}
+                      value={2}
+                    />
+                    <BaseButton
+                      clickAction={() => {
+                        this.setDestinationFloor(1);
+                        this.floorHandler();
+                      }}
+                      value={1}
+                    />
+                    <BaseButton
+                      clickAction={() => {
+                        this.setDestinationFloor(0);
+                        this.floorHandler();
+                      }}
+                      value={"G0"}
+                    />
+                  </div>
+                  <div className="elevator__halfNumbers1">
+                    <BaseButton
+                      clickAction={() => {
+                        this.setDestinationFloor(5);
+                        this.floorHandler();
+                      }}
+                      value={5}
+                    />
+                    <BaseButton
+                      clickAction={() => {
+                        this.setDestinationFloor(4);
+                        this.floorHandler();
+                      }}
+                      value={4}
+                    />
+                    <BaseButton
+                      clickAction={() => {
+                        this.setDestinationFloor(3);
+                        this.floorHandler();
+                      }}
+                      value={3}
+                    />
+                  </div>
+                </div>
+                <br />
+                <BaseButton
+                  color={"red"}
+                  clickAction={() => this.emergencyMessage()}
+                  value={"EMERGENCY"}
+                  baseButtonBorderRadius={"5%"}
+                />
+                <BaseButton
+                  color={"green"}
+                  clickAction={() => this.resetMessage()}
+                  value={"RESET MESSAGES"}
+                  baseButtonBorderRadius={"5%"}
+                />
+              </div>
             </div>
           </div>
         </div>
